49565c7140da8d7e0ec1009314f7352156ec02e9
hadarminei <59529156+hadarminei@users.noreply.github.com>
2021-08-19 13:12:12 +0300

    Added more windows

---
+++ app/src/main/java/com/postpc/dish/RateRecommendationViewModel.java
@@ -0,0 +1,7 @@
+package com.postpc.dish;
+
+import androidx.lifecycle.ViewModel;
+
+public class RateRecommendationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/postpc/dish/rate_recommendation.java
@@ -0,0 +1,36 @@
+package com.postpc.dish;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class rate_recommendation extends Fragment {
+
+    private RateRecommendationViewModel mViewModel;
+
+    public static rate_recommendation newInstance() {
+        return new rate_recommendation();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rate_recommendation_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RateRecommendationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
